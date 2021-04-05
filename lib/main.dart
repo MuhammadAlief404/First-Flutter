849b8283719c8567b5ed9114eb1f2807c8ed99bb
Muhammad Alief <alief.pribadi@gmail.com>
2021-04-05 18:22:52 +0800

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:first_flutter_app/ui/Home.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Opem Muhammad Alief porto',
+      theme: ThemeData(
+          primarySwatch: Colors.blue,
+          fontFamily: "Arial",
+          textTheme: TextTheme(
+              button: TextStyle(color: Colors.white, fontSize: 18.0),
+              title: TextStyle(color: Colors.red))),
+      home: Home(),
+    );
+  }
+}
